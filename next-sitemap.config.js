4758392e10a6b646eef8456406c6aa79f9d1e6d6
DevRGD <gautm.das@gmail.com>
2025-03-22 02:20:56 +0530

    fixed seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://manage-your-office.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  generateIndexSitemap: true,
+  exclude: ['/admin', '/dashboard'],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'Googlebot', allow: '/' },
+    ],
+    additionalSitemaps: ['https://manage-your-office.vercel.app/sitemap.xml'],
+  },
 };
