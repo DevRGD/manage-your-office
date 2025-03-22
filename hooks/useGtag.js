f0c355b305495690fd50d7f46379d75a4f69a253
DevRGD <gautm.das@gmail.com>
2025-03-22 17:43:15 +0530

    fixed useGtag

--- hooks/useGtag.js
+++ hooks/useGtag.js
@@ -1,20 +1,24 @@
 'use client';
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const useGtag = (GA_TRACKING_ID) => {
   const router = useRouter();
+
   useEffect(() => {
     const handleRouteChange = (url) => {
-      window.gtag('config', GA_TRACKING_ID, {
-        page_path: url,
-      });
+      if (typeof window.gtag !== 'undefined') {
+        window.gtag('config', GA_TRACKING_ID, {
+          page_path: url,
+        });
+      }
     };
-    router.events.on('routeChangeComplete', handleRouteChange);
+
+    router.events?.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events?.off('routeChangeComplete', handleRouteChange);
     };
-  }, [router]);
+  }, [router, GA_TRACKING_ID]);
 };
 
 export default useGtag;
